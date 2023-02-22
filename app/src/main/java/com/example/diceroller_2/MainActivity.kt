c3a2641ec911dde893cc561fdddd1ffb06bfc979
minhphi <himinhpho@gmail.com>
2023-02-22 15:45:31 +0700

    update

--- app/src/main/java/com/example/diceroller_2/MainActivity.kt
+++ app/src/main/java/com/example/diceroller_2/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller_2
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -15,8 +15,18 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener{
             val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
             toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = (1..6).random().toString()
+            val resultImageView: ImageView = findViewById(R.id.imageView)
+            val value = (1..6).random()
+            resultImageView.setImageResource(when (value){
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> R.drawable.dice_1
+            })
+
 
         }
 
